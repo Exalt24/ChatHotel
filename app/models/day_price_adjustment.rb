1e8f955e045ebf7b7b287a082ef81e600f8f72e5
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-07-03 19:11:14 +0800

    Add existing project files to Git

--- app/models/day_price_adjustment.rb
+++ app/models/day_price_adjustment.rb
@@ -1,5 +1,5 @@
 # app/models/day_price_adjustment.rb
 class DayPriceAdjustment < ApplicationRecord
     validates :day_of_week, presence: true, uniqueness: true
-    validates :price_adjustment, presence: true, numericality: { only_integer: true }
+    validates :price_adjustment, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -9,22 +9,37 @@ class Hotel < ApplicationRecord
     validates :contact_details, presence: true, format: { with: VALID_MOBILE_REGEX, message: "must be a valid Philippine mobile number starting with 0" }
     validates :single_room_price, :double_room_price, :suite_price, presence: true
     validate :room_price_order
+    validate :positive_room_prices
 
     private
 
-        def room_price_order
-            if single_room_price.present? && double_room_price.present? && suite_price.present?
-            if single_room_price >= double_room_price
-                errors.add(:single_room_price, "must be less than double room price")
-            end
-
-            if double_room_price >= suite_price
-                errors.add(:double_room_price, "must be less than suite price")
-            end
-
-            if single_room_price >= suite_price
-                errors.add(:single_room_price, "must be less than suite price")
-            end
-            end
+    def room_price_order
+      if single_room_price.present? && double_room_price.present? && suite_price.present?
+        if single_room_price >= double_room_price
+          errors.add(:single_room_price, "must be less than double room price")
         end
+
+        if double_room_price >= suite_price
+          errors.add(:double_room_price, "must be less than suite price")
+        end
+
+        if single_room_price >= suite_price
+          errors.add(:single_room_price, "must be less than suite price")
+        end
+      end
+    end
+
+    def positive_room_prices
+      if single_room_price <= 0
+        errors.add(:single_room_price, "must be greater than 0")
+      end
+
+      if double_room_price <= 0
+        errors.add(:double_room_price, "must be greater than 0")
+      end
+
+      if suite_price <= 0
+        errors.add(:suite_price, "must be greater than 0")
+      end
+    end
 end
