2180329061612c7d43e73fcd6fa154774565401b
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-07-03 18:05:40 +0800

    Add existing project files to Git

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,19 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install all required gems specified in Gemfile.lock
 bundle install
-
-# Precompile assets for production
 bundle exec rails assets:precompile
-
-# Clean up assets after precompilation (optional, but can be useful to reduce disk space usage)
 bundle exec rails assets:clean
-
-# Remove test files
-echo "Cleaning up test files..."
-rm -rf test/
-rm -rf spec/
-
-# Run database migrations
-bundle exec rails db:migrate
